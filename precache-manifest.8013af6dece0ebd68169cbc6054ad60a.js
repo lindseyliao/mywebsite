13bedd8e8ddbd8eb2d5ecda1c3ef38127c9923d7
lindseyliao <Lindseyliao1@gmail.com>
2020-08-21 13:34:11 +1000

    Updates

---
+++ precache-manifest.8013af6dece0ebd68169cbc6054ad60a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093c5a82ab66beec5b2932ba34c57400",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cb9c0df8109ab763d6f",
+    "url": "/static/css/main.7745ab64.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb9c0df8109ab763d6f",
+    "url": "/static/js/main.b0b86aab.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
