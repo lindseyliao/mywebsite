a2d7e0e90088c993d1ee290c07138e488a867a84
lindseyliao <Lindseyliao1@gmail.com>
2020-09-14 21:30:02 +1000

    save milestone 14 Sep Add business list

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './App.css';
+import BusinessList from '../Business/Business';
+import SearchBar from '../SearchBar/SearchBar';
+
+function App() {
+  return <div className="App">
+    <h1>ravenous</h1>
+    <SearchBar />
+    <BusinessList />
+    <BusinessList />
+    <BusinessList />
+  </div>
+}
+
+export default App;
